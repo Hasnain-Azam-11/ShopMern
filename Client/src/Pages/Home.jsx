0496380912e608f2971f369cdb25bf5c9d1b5efc
hasnain-fts <hasnainnain7711@gmail.com>
2026-07-24 16:05:02 +0500

    removed error

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -6,7 +6,6 @@ import { Badge } from "@/Components/ui/badge";
 import { ArrowRight, Search } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 const categories = [
   { name: "All" },
   { name: "shirts" },
